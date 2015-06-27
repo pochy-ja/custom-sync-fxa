fbb8f14ab903a546a636e0c2e04f7c6d10b2c3de
Pochy <jaaguilera@uci.cu>
2015-06-27 01:47:39 -0400

    Support for Content Server!

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ Add-on to customize server addresses for Server Sync and Firefox Accounts.
 
 Changelod
 
+0.1.5 Support for Server Content!
+
 0.1.3 Server Sync and Auth, code complete, yey!
 
 0.1.0 - Firsts commit
--- index.js
+++ index.js
@@ -19,7 +19,7 @@ sp.on("schemasync", onPrefSync);
 // Server Auth
 
 function onPrefAuth(){
-  if ( sp.prefs["useauth"]== true) {
+  if (sp.prefs["useauth"]== true) {
     set("identity.fxaccounts.auth.uri", sp.prefs["schemauth"]+"://"+sp.prefs["urlauth"]+"/v1");
   }
   else{
@@ -30,3 +30,23 @@ function onPrefAuth(){
 sp.on("useauth", onPrefAuth);
 sp.on("urlauth", onPrefAuth);
 sp.on("schemauth", onPrefAuth);
+
+// Server Content
+
+function onPrefContent(){
+  if (sp.prefs["usecontent"] == true) {
+    set("identity.fxaccounts.remote.signin.uri", sp.prefs["schemacontent"]+"://"+sp.prefs["urlcontent"]+"/signin?service=sync&context=fx_desktop_v1");
+    set("identity.fxaccounts.remote.signup.uri", sp.prefs["schemacontent"]+"://"+sp.prefs["urlcontent"]+"/signup?service=sync&context=fx_desktop_v1");
+    set("identity.fxaccounts.remote.force_auth.uri", sp.prefs["schemacontent"]+"://"+sp.prefs["urlcontent"]+"/force_auth?service=sync&context=fx_desktop_v1");
+    set("identity.fxaccounts.settings.uri", sp.prefs["schemacontent"]+"://"+sp.prefs["urlcontent"]+"/settings");
+  }
+  else{
+    reset("identity.fxaccounts.remote.signin.uri");
+    reset("identity.fxaccounts.remote.signup.uri");
+    reset("identity.fxaccounts.remote.force_auth.uri");
+    reset("identity.fxaccounts.settings.uri");
+  }
+}
+sp.on("usecontent", onPrefContent);
+sp.on("urlcontent", onPrefContent);
+sp.on("schemacontent", onPrefContent);
